f953d5b623a71da61655b6802b6be614127e19df
jilherme <rosa.guilhermediogo@gmail.com>
2023-06-14 21:57:36 -0300

    fix: middleware

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,13 +8,8 @@ const Header = () => {
   const user = session?.user;
 
   return (
-    <header className="bg-white h-20">
+    <header className="bg-black h-20">
       <nav className="h-full flex justify-between container items-center">
-        <div>
-          <Link href="/" className="text-ct-dark-600 text-2xl font-semibold">
-            CodevoWeb
-          </Link>
-        </div>
         <ul className="flex items-center gap-4">
           <li>
             <Link href="/" className="text-ct-dark-600">
